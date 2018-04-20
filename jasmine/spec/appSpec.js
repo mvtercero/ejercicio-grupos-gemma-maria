1e78ed1335d23365e1648f820bcde20b86f4229d
mvtercero <mvtercero85@gmail.com>
2018-04-20 15:01:03 +0200

    First test funtionality clean value

--- jasmine/spec/appSpec.js
+++ jasmine/spec/appSpec.js
@@ -1,8 +1,9 @@
-    //Describe anidado
     
     describe("App groups", function() { 
             it("adds new user", function() {
-                expect("Maria" + " " + "Vedia").toEqual("Maria Vedia");
+                var boxName = document.getElementById("box-name1").value;
+                var user1 = document.getElementsByClassName("user1");
+                expect(boxName).toEqual(""); 
             });
 
     });
